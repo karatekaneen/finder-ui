9a884cbcf8c3f9de36dcee16c6b9eda4bdfcb967
anders0228 <47074486+anders0228@users.noreply.github.com>
2019-05-30 15:23:21 +0200

    Dynamisk huvudmeny med login-hantering. Menyn har olika utseende om man är inloggad eller ej.

---
+++ finder-ui/Models/NavbarPartialViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace finder_ui.Models
+{
+    public class NavbarPartialViewModel
+    {
+        public bool LoggedIn { get; set; }
+        public string Username { get; set; }
+        public int UserId { get; set; }
+        public string UserPicture { get; set; }
+        public string DemoData { get; set; }
+    }
+}
