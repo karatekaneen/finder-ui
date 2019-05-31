8b4b88868c4c569c00ad16fa985fc81f0c6d9dba
Christoffer Andersson <31024266+junkoffer@users.noreply.github.com>
2019-05-31 16:43:43 +0200

    Lagt till validering Snyggat till valideringsmeddelanden Buggfixat Inloggning möjlig med epost ELLER användarnamn

--- finder-ui/Controllers/AccountController.cs
+++ finder-ui/Controllers/AccountController.cs
@@ -28,13 +28,15 @@ namespace finder_ui.Controllers
         public ActionResult CreateAccount(CreateAccountViewModel vm)
         {
             bool UsernameExist = true;
+            bool EmailExist = true;
             using (var client = new UserProfileServiceReference.UserProfileServiceClient())
             {
                 using (var validation = new UserLoginServiceReference.LoginServiceClient())
                 {
                     UsernameExist = validation.UsernameExist(vm.username);
+                    EmailExist = validation.EmailExist(vm.email);
                 }
-                if (!UsernameExist)
+                if (!UsernameExist && !EmailExist)
                 {
                     var newUser = new UserProfileServiceReference.NewUser()
                     {
@@ -68,7 +70,7 @@ namespace finder_ui.Controllers
                 }
                 else
                 {
-                    ViewBag.Message = "Användarnamnet finns redan";
+                    ViewBag.Message = "Det angivna användarnamnet eller eposten finns redan registrerat";
                     return View("Index");
                 }
 
@@ -120,43 +122,54 @@ namespace finder_ui.Controllers
         [HttpPost]
         public ActionResult UpdateUserProfile(UpdateProfileViewModel vm)
         {
-            using (var client = new UserProfileServiceReference.UserProfileServiceClient())
+
+            if (!ModelState.IsValid)
             {
-
-
-                int.TryParse(Session["UserId"].ToString(), out int userid);
-
-                var Userinfo = client.GetUserByUserId(userid);
-                var updateUser = new UserProfileServiceReference.User()
-                {
-                    Address = vm.userAddress,
-                    City = vm.userCity,
-                    PersonalCodeNumber = vm.personalnumber,
-                    Phonenumber = vm.userPhoneNumber,
-                    Picture = vm.userProfilePicture,
-                    ZipCode = vm.userZipCode,
-                    Id = userid,
-                    Email = Userinfo.Email,
-                    Name = Userinfo.Name,
-                    Surname = Userinfo.Surname,
-                    Username = Userinfo.Username,
-                };
-
-                var user = client.UpdateUser(updateUser);
-
+                return View(vm);
             }
-
             using (var client = new UserLoginServiceReference.LoginServiceClient())
             {
                 if (client.UsernameExist(vm.username))
                 {
                     ViewBag.Message = "Det valda Användarnamnet finns redan";
                 }
+                else if (client.EmailExist(vm.email))
+                {
+                    ViewBag.Message = "Den angiva epost adressen finns redan";
+                }
                 else
                 {
+                    using (var profileClient = new UserProfileServiceReference.UserProfileServiceClient())
+                    {
+
+
+                        int.TryParse(Session["UserId"].ToString(), out int userid);
+
+                        var Userinfo = profileClient.GetUserByUserId(userid);
+                        var updateUser = new UserProfileServiceReference.User()
+                        {
+                            Address = vm.userAddress,
+                            City = vm.userCity,
+                            PersonalCodeNumber = vm.personalnumber,
+                            Phonenumber = vm.userPhoneNumber,
+                            Picture = vm.userProfilePicture,
+                            ZipCode = vm.userZipCode,
+                            Id = userid,
+                            Email = Userinfo.Email,
+                            Name = Userinfo.Name,
+                            Surname = Userinfo.Surname,
+                            Username = Userinfo.Username,
+                        };
+
+                        var user = profileClient.UpdateUser(updateUser);
+
+
+                    }
                     return RedirectToAction("Index", "UserProfile");
                 }
+
             }
+
             return View();
         }
 
